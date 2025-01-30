113fbb03e568783e135a7c80cdedf04b73eb7c38
renu-aayush-maddi <2200032294@kluniversity.in>
2025-01-30 11:38:30 +0530

    Changes done in deployment urls

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL:"https://coin-shop-y1lg.vercel.app/api",
     withCredentials: true,
 })
 
