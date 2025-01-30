7ed44478a2ce6aebf9d0c4b48fcbc575c99d1369
renu-aayush-maddi <2200032294@kluniversity.in>
2025-01-30 11:55:05 +0530

    Changes done in Backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,7 +9,6 @@ import couponRoutes from "../routes/coupon.route.js"
 import paymentRoutes from "../routes/payment.route.js"
 import analyticsRoutes from "../routes/analytics.route.js"
 import cors from "cors"
-import path from "path";
 
 
 
@@ -18,12 +17,13 @@ const app = express()
 dotenv.config();
 const PORT = process.env.PORT || 5000
 
-const __dirname = path.resolve();
-
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
-app.use(cors({ origin: 'https://coin-shop.vercel.app' }));
+app.use(cors({
+	origin: 'https://coin-shop.vercel.app',
+	credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  }));
 app.use("/api/auth" , authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart", cartRoutes)
@@ -33,14 +33,6 @@ app.use("/api/analytics",analyticsRoutes)
 
 
 
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 
 
