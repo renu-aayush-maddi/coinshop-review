08f99544155207580665d0f36f5e529283c1c9fb
renu-aayush-maddi <2200032294@kluniversity.in>
2025-01-30 11:43:21 +0530

    Changes done in Backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -23,7 +23,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use("/api/auth" , authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart", cartRoutes)
