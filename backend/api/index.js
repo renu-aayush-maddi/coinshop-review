59948a3cac14fc5af1cf9edfc6d3b9ff861ec8ba
renu-aayush-maddi <2200032294@kluniversity.in>
2025-01-30 11:45:26 +0530

    Changes done in Backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -23,7 +23,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: 'https://coin-shop.vercel.app' }));
 app.use("/api/auth" , authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart", cartRoutes)
