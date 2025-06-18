a0a94a01cbbab51191fdb299666ad7de060507f7
renu-aayush-maddi <2200032294@kluniversity.in>
2025-06-18 08:16:45 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,12 @@ const app = express()
 dotenv.config();
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
+
 app.use(cors());
 app.use("/api/auth" , authRoutes)
 app.use("/api/products",productRoutes)
@@ -33,7 +36,13 @@ app.use("/api/reviews", reviewRoutes);
 
 
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 
